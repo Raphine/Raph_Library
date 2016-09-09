1847ca4c47d796f9db3b113dd6b0a4d6d94edf86
hikalium <hikalium@users.noreply.github.com>
2016-09-09 17:42:06 +0000

    Impl cpu.h

--- rlib/cpu.h
+++ rlib/cpu.h
@@ -25,10 +25,22 @@
 
 class CpuCtrlInterface {
 public:
+  enum CpuPurposes {
+    kNone = 0,
+	kLowPriority,
+	kGeneralPurpose,
+	kHighPerformance,
+	kCpuPurposesLen
+  };
+  const int kCpuIdNotFound = -1;
+  const int kCpuIdBootProcessor = 0;
+  //
   virtual ~CpuCtrlInterface() {
   }
   virtual volatile int GetId() = 0;
   virtual int GetHowManyCpus() = 0;
+  virtual int RetainCpuIdForPurpose(CpuPurposes p) = 0;
+  virtual void ReleaseCpuId(int cpuid) = 0;
   bool IsValidId(int cpuid) {
     return (cpuid >= 0 && cpuid < GetHowManyCpus());
   }
@@ -40,13 +52,73 @@ public:
 
 class CpuCtrl : public CpuCtrlInterface {
 public:
-  virtual volatile int GetId() override {
+  CpuCtrl() {
+    _cpu_purpose_map[0] = kLowPriority;
+  }
+  volatile int GetId() override {
     return apic_ctrl->GetCpuId();
   }
-  virtual int GetHowManyCpus() override {
+  int GetHowManyCpus() override {
     return apic_ctrl->GetHowManyCpus();
   }
+  int RetainCpuIdForPurpose(CpuPurposes p) override {
+    if(p == kLowPriority) return kCpuIdBootProcessor;
+      // boot processor is always assigned to kLowPriority
+    int cpuid;
+    cpuid = _GetCpuIdNotAssigned();
+    if(cpuid != kCpuIdNotFound){
+      _RetainCpuId(cpuid, p);
+      return cpuid;
+    }
+    cpuid = _GetCpuIdLessAssignedFor(p);
+    if(cpuid != kCpuIdNotFound){
+      _RetainCpuId(cpuid, p);
+      return cpuid;
+    }
+    return 0;
+  }
+  void ReleaseCpuId(int cpuid) override {
+    _ReleaseCpuId(cpuid);
+  }
+private:
+  CpuPurposes _cpu_purpose_map[ApicCtrl::lapicMaxNumber];
+  int _cpu_purpose_count[ApicCtrl::lapicMaxNumber];
+  // do not count for cpuid:0 (boot processor is always assigned to kLowPriority)
+  int _GetCpuIdNotAssigned(){
+    int i, len;
+    len = GetHowManyCpus();
+    for(i = 0; i < len; i++){
+      if(_cpu_purpose_map[i] == kNone) return i;
+    }
+    return kCpuIdNotFound;
+  }
+  int _GetCpuIdLessAssignedFor(CpuPurposes p){
+    int i, len, minCount = -1, minId = kCpuIdNotFound;
+    len = GetHowManyCpus();
+    for(i = 0; i < len; i++){
+      if(_cpu_purpose_map[i] == p &&
+		(minCount == -1 || _cpu_purpose_count[i] < minCount)){
+        minCount = _cpu_purpose_count[i];
+		minId = i;
+      }
+    }
+    return minId;
+  }
+  void _RetainCpuId(int cpuid, CpuPurposes p){
+    if(_cpu_purpose_map[cpuid] != p){
+      _cpu_purpose_map[cpuid] = p;
+      _cpu_purpose_count[cpuid] = 0;
+    }
+    _cpu_purpose_count[cpuid]++;
+  }
+  void _ReleaseCpuId(int cpuid){
+    if(_cpu_purpose_count[cpuid] > 0) _cpu_purpose_count[cpuid]--;
+    if(_cpu_purpose_count[cpuid] == 0){
+      _cpu_purpose_map[cpuid] = kNone;
+    }
+  }
 };
+
 #else
 #include <thread.h>
 #endif /* __KERNEL__ */
